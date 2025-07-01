1cbf29b17adcc20f7fcf711b5539ddfe334d4116
Louis <louis.pasquier@edu.hefr.ch>
2025-07-01 20:59:12 +0200

    feat: basic website template, add terminal component at the bottom of the page

---
+++ README.md
@@ -0,0 +1,15 @@
+# ✨ Portfolio ✨
+
+Hi ! I am Louis Pasquier and this is my portfolio.
+
+🚧⚠️ This portfolio is currently under development ⚠️🚧
+
+# ℹ️ Info
+
+Run the project :
+
+    npm run dev
+
+Build the project :
+    
+    npm run build
---
+++ src/components/Terminal.tsx
@@ -0,0 +1,130 @@
+import * as React from "react";
+import {ResizableBox} from "react-resizable";
+import "react-resizable/css/styles.css";
+import {type ChangeEvent, type KeyboardEvent, useEffect, useRef, useState} from "react";
+
+export default function Terminal() {
+
+    const [path, setPath] = useState('');
+    const newCommand: string = 'user:~'+path+'$ ';
+    const historyEndRef = useRef<null | HTMLDivElement>(null)
+
+    const [history, setHistory] = useState([
+        'Welcome to my portfolio',
+        '',
+        'Use this terminal to navigate in the website',
+        'Type help to check out the commands',
+        ''
+    ]);
+
+    const [content, setContent] = useState('');
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [history]);
+
+    const scrollToBottom = () => {
+        historyEndRef.current?.scrollIntoView(); // not sure if it's better with { behavior: "smooth" }
+    }
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setContent(event.target.value);
+    };
+
+    const handleInputSubmit = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            parseCommand();
+            setHistory([...history, newCommand + content]);
+            setContent('');
+        }
+    };
+
+    const parseCommand = () => {
+        const tokens: string[] = content.split(' ');
+        switch (tokens[0]) {
+            case 'cd':
+                handleCd(tokens.slice(1));
+                break;
+        }
+    }
+
+    const handleCd = (tokens: string[])=>  {
+        setPath(tokens[0]);
+    }
+
+    return (
+        <div style={resizeBoxWrapper}>
+            <ResizableBox width={window.innerWidth} height={200} minConstraints={[window.innerWidth, 100]} maxConstraints={[window.innerWidth, 300]}
+                          resizeHandles={['n']} axis="y" style={resizeBox} handle={<span className="custom-handle custom-handle-n" />}>
+                <div style={contentWrapper}>
+                    <div style={contentStyle}>
+                        {history.map((line, key) => (
+                            <span key={key}>
+                                {line}
+                                <br/>
+                            </span>
+                        ))}
+                        {newCommand}
+                        <input
+                            style={inputStyle}
+                            type="text"
+                            value={content}
+                            onChange={handleInputChange}
+                            onKeyDown={handleInputSubmit}
+                        />
+                        <div ref={historyEndRef} />
+                    </div>
+                </div>
+
+            </ResizableBox>
+        </div>
+    );
+}
+
+const terminalColor: string = '#202020';
+const terminalPolice: string = 'Consolas, monospace';
+
+const resizeBoxWrapper: React.CSSProperties = {
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    zIndex: 1000,
+};
+
+const resizeBox : React.CSSProperties = {
+    width: '100%',
+    height: '100%',
+    padding: '1%',
+    backgroundColor: terminalColor,
+    fontFamily: terminalPolice,
+    position: "relative",
+    overflow: 'hidden',
+    boxSizing: 'border-box',
+};
+
+const contentWrapper: React.CSSProperties = {
+    height: "100%",
+    overflowY: "auto",
+    paddingTop: 8,
+};
+
+const contentStyle: React.CSSProperties = {
+    color: "white",
+    padding: "0 1rem",
+    textAlign: 'left',
+};
+
+const inputStyle: React.CSSProperties = {
+    WebkitAppearance: 'none',
+    border: 'none',
+    outline: 'none',
+    boxShadow: 'none',
+
+    backgroundColor: terminalColor,
+    fontFamily: terminalPolice,
+    lineHeight: 1.5,
+    fontWeight: 400,
+    fontSize: 16,
+    width: "90%",
+}
