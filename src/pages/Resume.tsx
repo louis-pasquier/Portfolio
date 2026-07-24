0b22647160f2d182beaca9f1e1da3cb06ee2274d
Louis <louis.pasquier@outlook.com>
2026-07-24 21:15:31 +0200

    feat: add favicon, smoother loading of images and pdf, deploy pipeline

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,17 +1,24 @@
 import resumeUrl from '../assets/resume/resume.pdf';
-import { memo } from 'react';
+import { memo, useState } from 'react';
 
 const Resume = memo(function Resume() {
+    const [isLoading, setIsLoading] = useState(true);
+
     return (
-        <object
-            data={resumeUrl}
-            type="application/pdf"
-            width="100%"
-            height="100%"
-        >
-            <p>My resume</p>
-        </object>
+        <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            {isLoading && <p>Loading resume...</p>}
+            <object
+                data={resumeUrl}
+                type="application/pdf"
+                width="100%"
+                height="100%"
+                onLoad={() => setIsLoading(false)}
+                style={{ display: isLoading ? 'none' : 'block' }}
+            >
+                <p>My resume</p>
+            </object>
+        </div>
     );
 });
 
-export default Resume
+export default Resume;
